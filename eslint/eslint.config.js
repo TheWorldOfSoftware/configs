339c8e78c0a255186a950e21190656d6479f889d
Scraps <123112608+ScrapsPrograms@users.noreply.github.com>
2024-08-24 11:31:06 +0200

    Dropped eslint-plugin-import as it doesn't work with symlinks (pnpm)
    Signed-off-by: Scraps <123112608+ScrapsPrograms@users.noreply.github.com>

--- eslint/eslint.config.js
+++ eslint/eslint.config.js
@@ -1,5 +1,4 @@
 import eslint from "@worldofsoftware/eslint-config-javascript";
 import typescriptEslint from "@worldofsoftware/eslint-config-typescript";
-import eslintImport from "@worldofsoftware/eslint-config-import";
 
-export default [eslint, ...typescriptEslint, eslintImport];
+export default [eslint, ...typescriptEslint];
